32787f834097c34a529862abddfbfc845f193bb7
JP <54604238+tgrtgak@users.noreply.github.com>
2020-11-18 10:29:06 -0500

    Tweaked chaser acceleration

--- enemies.cpp
+++ enemies.cpp
@@ -245,11 +245,15 @@ void Enemy::update(Tilemap* t, double delta_time, double playerX, double playerY
 		{
 			if (playerX + 7 < this->x)
 			{
+				if (xVel > 0)
+					xVel = 1.5;
 				xVel -= accel * delta_time;
 				setCurrFrame(0);
 			}
 			else if (playerX + 7 > this->x)
 			{
+				if (xVel < 0)
+					xVel = -1.5;
 				xVel += accel * delta_time;
 				setCurrFrame(1);
 			}
@@ -258,11 +262,15 @@ void Enemy::update(Tilemap* t, double delta_time, double playerX, double playerY
 		{
 			if (playerX + 7 < this->x)
 			{
+				if (xVel < 0)
+					xVel = -1.5;
 				xVel += accel * delta_time;
 				setCurrFrame(3);
 			}
 			else if (playerX + 7 > this->x)
 			{
+				if (xVel > 0)
+					xVel = 1.5;
 				xVel -= accel * delta_time;
 				setCurrFrame(2);
 			}
